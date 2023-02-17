9ed8f487cd4327b656722627955970be325ecb17
donnah clara <donnahclara@gmail.com>
2023-02-17 04:12:56 -0800

    Puts code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Program that prints with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
